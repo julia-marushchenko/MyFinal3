1aef5c6fe71afe41752ee2e3c0b5913af27facee
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-11-19 18:29:45 +0100

    Add files via upload
    Committing Main and Main1 class

---
+++ src/com/finals/Main.java
@@ -0,0 +1,19 @@
+/**
+ * Java program to demonstrate final classes can not be extended.
+ */
+
+package com.finals;
+
+/**
+ * Main class
+ */
+public final class Main {
+}
+
+
+/**
+ * Main class
+ */
+class Main1 extends Main1 {
+    // Compilation ERROR
+}
